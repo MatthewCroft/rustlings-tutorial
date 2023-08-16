55ee9d9b8472a685b7469cbbe2598ff7bca6ec2f
Matthew Croft <matthewpcroft1@gmail.com>
2023-08-16 10:44:15 -0400

    primitive datatypes

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,14 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    // then ending_index is +1 past the slice of where you want
+    // internally rust gets the slice by startIndex..(endIndex-startIndex)
+    // hence 4-1 = 3, then length of [2,3,4]
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
