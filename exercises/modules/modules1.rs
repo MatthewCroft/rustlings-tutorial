052ea9e1dd4ed42e68f3f0a43cf86065d4a68ca4
Matthew Croft <matthewpcroft1@gmail.com>
2023-08-18 23:24:37 -0400

    modules, hashmaps

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,20 +1,22 @@
 // modules1.rs
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
-        println!("sausage!");
+    pub fn make_sausage() {
+        let secret_recipe = get_secret_recipe();
+        println!("{}", format!("{secret_recipe} sausage!"));
     }
 }
 
+// main is same level as sausage_factory so no need for the mod to be public
+// make_sausage needs to be public so it can be accessed by its parent,
+// notice get_secret_recipe is private, this is because by default
+// children have access to their ancestors
 fn main() {
     sausage_factory::make_sausage();
 }
